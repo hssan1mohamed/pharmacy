7352aad07a7e2399cdf98a76a22220659eb4c710
hassan <hassan02468@yahoo.com>
2022-04-17 21:35:57 +0200

    profile screen

--- lib/shaerd/provider/provider.dart
+++ lib/shaerd/provider/provider.dart
@@ -1,5 +1,6 @@
 
  import 'package:flutter/cupertino.dart';
+import 'package:pharmacy/home_test.dart';
 import 'package:pharmacy/moduels/cart/cart.dart';
 import 'package:pharmacy/moduels/favorite/favorite.dart';
 import 'package:pharmacy/moduels/home/home.dart';
@@ -7,17 +8,29 @@ import 'package:pharmacy/moduels/profile/profile.dart';
 import 'package:pharmacy/moduels/search/search.dart';
 
 class MyProvider extends ChangeNotifier{
+  List<Widget> _favoriteList =[];
+  List <Widget> get favoriteList => _favoriteList;
+  void addToList(Widget prodect){
+    _favoriteList.add(prodect);
+    notifyListeners();
+  }
+  void removeFav(Widget prodect){
+    _favoriteList.remove(prodect);
+    notifyListeners();
+  }
   int currentIndex = 0;
   List <Widget> bootomScreen =[
-   HomeScreen(),
+    HomeTest(),
     FavoriteScreen(),
     CartScreen(),
     SearchScreen(),
     ProfileScreen(),
 
   ];
+
   void changeBottom(int index){
     currentIndex =index;
     notifyListeners();
   }
+
 }
