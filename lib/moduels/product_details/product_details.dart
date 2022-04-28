4fabc219c14be9a2b3dfc372fe52630bb5664bc7
hssan1mohamed <78236187+hssan1mohamed@users.noreply.github.com>
2022-04-28 04:26:12 +0200

    product_details_update

--- lib/moduels/product_details/product_details.dart
+++ lib/moduels/product_details/product_details.dart
@@ -11,17 +11,101 @@ class _product_detailsState extends State<product_details> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(children: [
-        Container(
-          height: height * 0.5,
-          width: double.infinity,
-        ),
-        Container(
-          height: height * 0.5,
-          width: double.infinity,
-        ),
-      ]),
+      appBar: AppBar(
+          leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: Icon(Icons.arrow_back, size: 30, color: Colors.white)),
+          centerTitle: true,
+          title: Text(
+            'التفاصيل ',
+            style: const TextStyle(
+                color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
+          )),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.all(25),
+            height: height * 0.3,
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Image.asset('assets/images/drug.png'),
+          ),
+          Container(
+            height: height * 0.5998,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              // color: Colors.cyan,
+              border: Border.all(color: Colors.cyan),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35), topRight: Radius.circular(35)),
+            ),
+            alignment: Alignment.topRight,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ListTile(
+                    trailing: Text(
+                      'اسبرين ',
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text(
+                      '15 \$ ',
+                      style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text(
+                      'السعر ',
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(),
+                  RaisedButton(
+                    color: Colors.white,
+                    shape: Border(),
+                    onPressed: () {},
+                    child: Text(
+                      'اضف الى العربة ',
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    trailing: Text(
+                      'التفاصيل ',
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(),
+                  Text(
+                      'لارترترترتؤاءالؤالمنتمنتمن\n رلرتارارلاؤلا\n الؤءابيالر ىلارءلبؤاللاي\n',
+                      style: TextStyle(color: Colors.cyan, fontSize: 18)),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
